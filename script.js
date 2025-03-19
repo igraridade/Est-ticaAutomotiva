840502dcce88806616ddf0f50f9dce4be6f5e13f
igraridade <S.blacweel@gmail.com>
2025-03-19 09:36:33 -0300

    19/03

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("titulo");
+
+console.log(title);
+
+const button = document.querySelectorAll("botao");
+
+console.log(button);
+
+const links = document.getElementsByClassName("link");
+
+console.log(links);
